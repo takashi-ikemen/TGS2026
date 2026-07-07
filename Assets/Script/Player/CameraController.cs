aba12ce6d9c225e6244eff3445cb7f8fdbd7255f
KCSNET\st20244157 <20244157@kcska.onmicrosoft.com>
2026-07-07 12:33:51 +0900

    3dオブジェクトの追加　

---
+++ Assets/Script/Player/CameraController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float sensitivity = 150f;
+
+    private Vector2 lookInput;
+    private float pitch;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        float mouseX = lookInput.x * sensitivity * Time.deltaTime;
+        float mouseY = lookInput.y * sensitivity * Time.deltaTime;
+
+        //ç∂âEÇÕPlayerÇâÒì]
+        player.Rotate(Vector3.up * mouseX);
+
+        //è„â∫ÇÕCameraPivotÇæÇØâÒì]
+        pitch -= mouseY;
+        pitch = Mathf.Clamp(pitch, -80f, 80f);
+
+        transform.localRotation = Quaternion.Euler(pitch, 0f, 0f);
+    }
+}
---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody rb;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 move =
+            transform.forward * moveInput.y +
+            transform.right * moveInput.x;
+
+        rb.MovePosition(
+            rb.position + move * moveSpeed * Time.fixedDeltaTime);
+    }
+}
